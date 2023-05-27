995410da11891c9c33bbcfb8e469363997221077
May <ADMIN@MAY>
2023-05-28 00:34:26 +0700

    Add project files.

---
+++ FinalTest_2/Controllers/MuahangController.cs
@@ -0,0 +1,38 @@
+﻿using kiemtra.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using FinalTest_2.Models;
+
+namespace FinalTest_2.Controllers
+{
+    public class MuahangController : Controller
+    {
+        public static List<HomeProduct> products = new List<HomeProduct>();
+        public IActionResult Sanpham(bool? check = false)
+        {
+            if ((bool)check)
+            {
+                products.Clear();
+            }
+            else
+            {
+                return View(products.ToList());
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Sanpham(string tensanpham, int soluong, int dongia, double giamgia)
+        {
+            var homeProduct = new HomeProduct(tensanpham, soluong, dongia, giamgia);
+            products.Add(homeProduct);
+            return View(products);
+        }
+
+        public IActionResult RemoveProduct(int position)
+        {
+            products.RemoveAt(position - 1);
+            return RedirectToAction("Sanpham", "Muahang");
+        }
+    }
+}
+
---
+++ FinalTest_2/Models/Sanpham.cs
@@ -0,0 +1,22 @@
+﻿namespace kiemtra.Models
+{
+    public class HomeProduct
+    {
+        public string TenSanPham { get; set; }
+        public int SoLuong { get; set; }
+        public int DonGia { get; set; }
+        public int TamTinh { get; set; }
+        public double Khuyenmai { get; set; }
+        public double Tongtien { get; set; }
+        public HomeProduct() { }
+        public HomeProduct(string tenSanPham, int soLuong, int gia, double giamgia)
+        {
+            TenSanPham = tenSanPham;
+            SoLuong = soLuong;
+            DonGia = gia;
+            TamTinh = soLuong * gia;
+            Khuyenmai = SoLuong * gia * (giamgia / 100);
+            Tongtien = TamTinh - Khuyenmai; 
+        }
+    }
+}
